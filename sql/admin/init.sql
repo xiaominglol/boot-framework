c2de0f671dabad5b4266f0e0b21a761e76c0701a
xiaominglol <474345633@qq.com>
2019-07-07 17:51:01 +0800

    目录和命名大整改

--- sql/admin/init.sql
+++ sql/admin/init.sql
@@ -2,6 +2,6 @@ create database if not exists boot_admin default character set utf8;
 
 CREATE USER 'boot_admin'@'%' IDENTIFIED BY 'boot_admin';
 
-grant all privileges on gemini_core.* to 'boot_admin'@'%' identified by 'boot_admin';
+grant all privileges on boot_admin.* to 'boot_admin'@'%' identified by 'boot_admin';
 
 flush privileges;
